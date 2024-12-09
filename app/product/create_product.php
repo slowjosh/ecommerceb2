d1784d653acc4264f205e316e08be9a386e91a03
joshua <joshuarelle.lucero@sdca.edu.ph>
2024-12-09 11:16:26 +0800

    payment confirmation complete

--- app/product/create_product.php
+++ app/product/create_product.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION)){
     session_start();
 }
 
-require_once(__DIR__."/../config/Directories.php"); //to handle folder specific path
+require_once(__DIR__."/../config/Directories.php"); 
 include("../config/DatabaseConnect.php"); //to access database connection
 
 $db = new DatabaseConnect(); //make a new database instance
